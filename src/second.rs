4971310d2e526166045e85b990996cb710b98936
OscarClemente <oscar.clemente@gmail.com>
2021-03-07 22:39:48 +0100

    Added iter() with life elision

--- src/second.rs
+++ src/second.rs
@@ -45,6 +45,10 @@ impl<T> List<T> {
   pub fn into_iter(self) -> IntoIter<T> {
     IntoIter(self)
   }
+
+  pub fn iter(&self) -> Iter<T> {
+    Iter {next: self.head.as_deref()}
+  }
 }
 
 impl<T> Drop for List<T> {
@@ -69,6 +73,21 @@ impl<T> Iterator for IntoIter<T> {
   fn next(&mut self) -> Option<Self::Item> {
     // access fields of a tuple struct numerically
     self.0.pop()
+  }
+}
+
+pub struct Iter<'a, T> {
+  next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+  type Item = &'a T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.map(|node| {
+      self.next = node.next.as_deref();
+      &node.elem
+    })
   }
 }
 
@@ -131,5 +150,16 @@ mod test {
       assert_eq!(iter.next(), Some(2));
       assert_eq!(iter.next(), Some(1));
       assert_eq!(iter.next(), None);
+  }
+
+  #[test]
+  fn iter() {
+      let mut list = List::new();
+      list.push(1); list.push(2); list.push(3);
+
+      let mut iter = list.iter();
+      assert_eq!(iter.next(), Some(&3));
+      assert_eq!(iter.next(), Some(&2));
+      assert_eq!(iter.next(), Some(&1));
   }
 }
